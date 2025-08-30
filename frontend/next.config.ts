cdb982d279cdad85373e0729d9a326f6d2719551
psu <peter.suepfle@gmail.com>
2025-08-30 08:06:46 +0200

    set nextjs config manually + disable turbopack for build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    outputFileTracingRoot: __dirname,
+    output: "export",
+    images: {
+        unoptimized: true, // safe for static export + GitHub Pages
+    },
+    basePath: "/",
+
 };
 
 export default nextConfig;
